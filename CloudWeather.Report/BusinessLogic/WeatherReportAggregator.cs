98897d6dfdec1712477e2e7e2c8bb3460c37db57
Douglas <douglas-cprdev@hotmail.com>
2023-05-01 18:04:00 -0300

    Development of weekly report

---
+++ CloudWeather.Report/BusinessLogic/WeatherReportAggregator.cs
@@ -0,0 +1,110 @@
+using System.Diagnostics;
+using System.Text.Json;
+using CloudWeather.Report.Config;
+using CloudWeather.Report.DataAccess;
+using CloudWeather.Report.Models;
+using Microsoft.Extensions.Options;
+
+namespace CloudWeather.Report.BusinessLogic;
+
+public interface IWeatherReportAggregator { 
+  public Task<WeatherReport> BuildWeeklyReport(string zip, int days);
+}
+
+public class WeatherReportAggregator : IWeatherReportAggregator
+{
+  private readonly IHttpClientFactory _http;
+  private readonly ILogger<WeatherReportAggregator> _logger;
+  private readonly WeatherDataConfig _weatherDataConfig;
+  private readonly WeatherReportDbContext _db;
+
+  public WeatherReportAggregator(
+    IHttpClientFactory http,
+    ILogger<WeatherReportAggregator> logger,
+    IOptions<WeatherDataConfig> weatherConfig,
+    WeatherReportDbContext db
+  ) 
+  {
+    _http = http;
+    _logger = logger;
+    _weatherDataConfig = weatherConfig.Value;
+    _db = db;
+  }
+
+  public async Task<WeatherReport> BuildWeeklyReport(string zip, int days)
+  {
+    var httpClient = _http.CreateClient();
+    var precipData = await FetchPrecipitationData(httpClient, zip, days);
+
+    var totalSnow = GetTotalSnow(precipData);
+    var totalRain = GetTotalRain(precipData);
+
+    _logger.LogInformation(
+      $"zip: {zip} over last {days} days: " +
+      $"total snow: {totalSnow}, rain: {totalRain}" 
+    );
+
+    var tempData = await FetchTemperatureData(httpClient, zip, days);
+  }
+
+  private async Task<List<TemperatureModel>> FetchTemperatureData(HttpClient httpClient, string zip, int days)
+  {
+    var endpoint = BuildTemperatureServiceEndpoint(zip, days);
+    var temperatureRecords = await httpClient.GetAsync(endpoint);
+    var jsonSerializerOptions = new JsonSerializerOptions {
+      PropertyNameCaseInsensitive = true,
+      PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+    var temperatureData = await temperatureRecords
+    .Content
+    .ReadFromJsonAsync<List<TemperatureModel>>(jsonSerializerOptions);
+    return temperatureData ?? new List<TemperatureModel>(); 
+  }
+
+  private async Task<List<PrecipitationModel>> FetchPrecipitationData(HttpClient httpClient, string zip, int days)
+  {
+    var endpoint = BuildPrecipitationServiceEndpoint(zip, days);
+    var precipRecords = await httpClient.GetAsync(endpoint);
+    var jsonSerializerOptions = new JsonSerializerOptions {
+      PropertyNameCaseInsensitive = true,
+      PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+    var precipData = await precipRecords
+    .Content
+    .ReadFromJsonAsync<List<PrecipitationModel>>(jsonSerializerOptions);
+    return precipData ?? new List<PrecipitationModel>(); 
+  }
+
+
+  private string BuildTemperatureServiceEndpoint(string zip, int days)
+  {
+    var tempServiceProtocol = _weatherDataConfig.TempDataProtocol;
+    var tempServiceHost = _weatherDataConfig.TempDataPort;
+    var tempServicePort = _weatherDataConfig.TempDataPort;
+    return $"{tempServiceProtocol}://{tempServiceHost}:{tempServicePort}/observation/{zip}?days={days}";
+  }
+
+    private string BuildPrecipitationServiceEndpoint(string zip, int days)
+  {
+    var precipServiceProtocol = _weatherDataConfig.PrecipDataProtocol;
+    var precipServiceHost = _weatherDataConfig.PrecipDataPort;
+    var precipServicePort = _weatherDataConfig.PrecipDataPort;
+    return $"{precipServiceProtocol}://{precipServiceHost}:{precipServicePort}/observation/{zip}?days={days}";
+  }
+
+  private static decimal GetTotalSnow(IEnumerable<PrecipitationModel> precipData)
+  {
+    var totalSnow = precipData
+      .Where(p => p.WeatherType == "snow")
+      .Sum(p => p.AmountInches);
+    return Math.Round(totalSnow, 1);
+  }
+
+  private static decimal GetTotalRain(IEnumerable<PrecipitationModel> precipData)
+  {
+    var totalRain = precipData
+      .Where(p => p.WeatherType == "rain")
+      .Sum(p => p.AmountInches);
+    return Math.Round(totalRain, 1);
+  }
+}
