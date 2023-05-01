662ad0c6693b2481a0c5466c933ea46a3c2b705a
Douglas <douglas-cprdev@hotmail.com>
2023-05-01 17:30:25 -0300

    Add endpoints for services

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -14,8 +14,6 @@ builder.Services.AddDbContext<PrecipDbContext>(
 
 var app = builder.Build();
 
-
-
 app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) => {
   if (days == null || days < 0 || days > 30) return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
   var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
@@ -26,5 +24,10 @@ app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, Preci
 });
 
 
+app.MapPost("/observation", async (Precipitation precip, PrecipDbContext db) => {
+  precip.CreatedOn = precip.CreatedOn.ToUniversalTime();
+  await db.AddAsync(precip);
+  await db.SaveChangesAsync();
+});
 
 app.Run();
